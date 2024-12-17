1abbcdb9b5792af4d419436e96b59d0fbf5f1017
David Miguel <david@brxsapp.com>
2024-12-18 00:07:30 +0100

    Add Anthropic provider

--- lib/src/providers/providers.dart
+++ lib/src/providers/providers.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+export 'implementations/anthropic_provider.dart';
 export 'implementations/echo_provider.dart';
 export 'implementations/gemini_provider.dart';
 export 'implementations/vertex_provider.dart';
